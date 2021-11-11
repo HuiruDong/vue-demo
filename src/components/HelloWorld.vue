c0a4de6fe9633feea3eb51e5a076cb19dedbf346
Huiru Dong <j-huirudong@microsoft.com>
2021-11-11 16:05:33 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="container">
+        <div
+            class="card"
+            v-for="item in renderCards"
+            :key="item"
+            :style="{ background: item }"
+        ></div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: 'HelloWorld',
+    props: {
+        msg: String,
+    },
+    data() {
+        return {
+            cards: [
+                'lightblue',
+                'lightcoral',
+                'lightgray',
+                'lightpink',
+                'lightsalmon',
+                'lightseagreen',
+            ],
+            renderCards: [],
+        };
+    },
+    created() {
+        this.renderCards = JSON.parse(JSON.stringify(this.cards));
+    },
+    mounted() {
+        this.changeCards(document.body.clientWidth);
+        window.onresize = () => {
+            this.changeCards(document.body.clientWidth);
+        };
+    },
+    methods: {
+        changeCards(width) {
+            if (width <= 1200) {
+                this.renderCards = JSON.parse(JSON.stringify(this.cards)).slice(0, 4);
+            } else if (width <= 1440) {
+                this.renderCards = JSON.parse(JSON.stringify(this.cards)).slice(0, 5);
+            } else {
+                this.renderCards = JSON.parse(JSON.stringify(this.cards));
+            }
+        },
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+    height: 200px;
+    display: flex;
+    flex-wrap: nowrap;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.container div {
+    flex: 1;
 }
 </style>
